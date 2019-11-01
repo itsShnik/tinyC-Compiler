4bcfb3f06e7f16fadddbf7fd44530b4c1a307229
Apoorve Singhal <sapoorve@outlook.com>
2019-11-01 16:38:16 +0000

    working makefile

--- ass6/ass6_16CS10034/ass6_17CS10030_17CS30007_test4.c
+++ ass6/ass6_16CS10034/ass6_17CS10030_17CS30007_test4.c
@@ -4,19 +4,23 @@
 int prints(char *c);
 int printi(int i);
 
-int inc(int a,int b){
-  prints("\n\nEntered inside the function body \n");
-  b=b+7+a;
+int hakku(int a,int b){
+  prints("\n\nEntered the body of hakku\n");
+  if (i > j) {
+    prints("returning value: ");
+    printi(a+3);
+    return (a + 3);
+  }
   prints("returning value: ");
-  printi(b);
-  return b;
+  printi(b+3)
+  return (b + 3);
 }
 
 int main() {
   int i,j=7;
-  i=1;
-  prints("+++++++++  Entering the function inc   +++++++ \n");
-  j=inc(i,j);
+  i=9;
+  prints("Testing function hakku - returns max(a,b) + 3\n");
+  j=hakku(i,j);
   prints("\n\n+++++++++   The value returned is   +++++++++ \n");
   printi(j);
   prints("\n");
